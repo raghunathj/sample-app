c8f44dfd1ff127ce0cbf5b1009bec98290fdf081
raghunathj <jrcbangalore@gmail.com>
2010-12-26 15:28:58 +0530

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
-  def home
+  def home	
+  @title ="home"
   end
 
   def contact
+  @title = "contact"
+  end
+
+  def about
+  @title = "About"
   end
 
 end
